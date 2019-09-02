cb6b9f1951c00bcb488a00f45b414e64e489945c
Abdelrahman ghrayeb <abedghrayeb@gmail.com>
2019-09-02 03:08:57 +0300

    Add project files.

---
+++ Data/DatingDbContext.cs
@@ -0,0 +1,16 @@
+﻿using DatingApp.API.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DatingApp.API.Data
+{
+    public class DatingDbContext:DbContext
+    {
+        public DatingDbContext(DbContextOptions<DatingDbContext> options) : base(options){ }
+
+        public DbSet<Value> Values { get; set; }
+    }
+}
